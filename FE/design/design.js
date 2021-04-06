f951f933a9059f9a78a354104c31141cfeef9daf
Tien <18521482@gm.uit.edu.vn>
2021-04-06 18:21:13 +0700

    update homepage

--- FE/design/design.js
+++ FE/design/design.js
@@ -6,10 +6,10 @@ $('.slider-container').slick({
   nextArrow: '.next-btn'
 });
 
-window.onscroll = () => {
+/*window.onscroll = () => {
   if (document.documentElement.scrollTop > innerHeight*0.5){
     document.querySelector('.nav').style.background = "url('./img/banner-bg.jpg') bottom";
-    document.querySelector('.logo > img').src = "./img/logo_trans.png";
+    document.querySelector('.logo > img').src = "./img/Edulogy-logos_transparent.png";
     let dropBtn = document.getElementsByClassName('dropdown-btn');
     for(let i = 0; i < dropBtn.length; i++){
       dropBtn[i].style.color = '#333'
@@ -23,17 +23,17 @@ window.onscroll = () => {
       for(let i = 0; i < dropBtn.length; i++){
         dropBtn[i].style.color = '#333'
       }
-      document.querySelector('.logo > img').src = "./img/logo_trans.png";
+      document.querySelector('.logo > img').src = "./img/Edulogy-logos_transparent.png";
       document.getElementById('login-btn').style.color = '#333';
     }else{
       for(let i = 0; i < dropBtn.length; i++){
         dropBtn[i].style.color = '#fff'
       }
-      document.querySelector('.logo > img').src = "./img/logo_trans_light.png";
+      document.querySelector('.logo > img').src = "./img/Edulogy-logos_transparent.png";
       document.getElementById('login-btn').style.color = '#fff';      
     }
   }
-}
+}*/
 
 function showLoginForm(){
   var modal = document.getElementsByClassName("app")[0];
@@ -44,3 +44,23 @@ function closeModalForm(){
   var modal = document.getElementsByClassName("app")[0];
   modal.style.display= 'none';
 }
+const nav = document.querySelector('.nav');
+let prevScrollpos = window.pageYOffset;
+
+window.addEventListener('scroll',()=>{
+  let currentScrollpos = window.pageYOffset;
+
+  if(prevScrollpos< currentScrollpos){
+    if(document.documentElement.scrollTop>100)
+    {
+      document.querySelector('.nav').style.background = "white";
+    }
+    nav.classList.add('hide');
+  }
+  else{
+    nav.classList.remove('hide');
+  }
+
+  prevScrollpos = currentScrollpos;
+}
+)
