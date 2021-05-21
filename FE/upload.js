6121be578300e883206b7dcd6c924accba2f6f7e
thailnv <56252864+thailnv@users.noreply.github.com>
2021-05-21 09:09:24 +0700

    remove media file

--- FE/upload.js
+++ FE/upload.js
@@ -3,7 +3,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
   console.log("fk");
   var form = document.getElementById("form");
   var formData = new FormData(form);
-  fetch("http://localhost:3000/api/tests", {
+  fetch("http://localhost:5000/api/tests", {
     method: "POST",
     body: formData,
   });
