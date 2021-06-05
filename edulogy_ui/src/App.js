1ef907bb92c18358aeccc25473493826fd17c7b5
Arina <66809935+18521537@users.noreply.github.com>
2021-06-05 12:28:47 +0700

    backup 2nd

--- edulogy_ui/src/App.js
+++ edulogy_ui/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/homepage';
+import DiscussionsPage from './pages/discussionsPage';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <HomePage/>
+      {/* <HomePage/> */}
+      <DiscussionsPage/>
     </BrowserRouter>
   );
 }
